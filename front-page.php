bfb121dcbc4bfbb78e27aa69e02a6f67e13f66a4
jgadsbypeet <jgadsbypeet@gmail.com>
2015-11-03 17:33:36 +0000

    Add in services to front-page

--- front-page.php
+++ front-page.php
@@ -2,4 +2,5 @@
 <?php get_template_part('menu'); ?>
 <?php get_template_part('jumbotron'); ?>
 <?php get_template_part('loop'); ?>
+<?php get_template_part('services'); ?>
 <?php get_footer(); ?>
---
+++ services.php
@@ -0,0 +1,9 @@
+<div class="full-width-grey">
+
+    <div class="container">
+    <div class="col-md-12">
+        sss
+        </div>
+    
+    </div>
+</div>
